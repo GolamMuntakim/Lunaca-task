865337effaaf6978fa048590b06b093b1e30c58f
Golam Muntakim <seams123456789@gmail.com>
2024-09-05 10:18:44 -0700

    design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { NavLink, Outlet } from "react-router-dom"
-import { FaRegQuestionCircle } from "react-icons/fa";
+import { FaArrowLeft, FaArrowRight, FaRegQuestionCircle } from "react-icons/fa";
 
 
 function App() {
@@ -47,7 +47,39 @@ function App() {
                 </div>
               </div>
             </div>
-            <div className="h-[270px] w-[550px] border-2 border-solid border-black rounded-xl mt-3 bg-[#373d43]"></div>
+            <div className="h-[270px] w-[550px] border-2 border-solid border-black rounded-xl mt-3 bg-[#373d43]">
+            <div className="grid grid-cols-3 w-[550px]">
+                <div className="w-[40px]">
+                  <div>
+                  <FaRegQuestionCircle className="text-[#99a0ae] text-xl mt-10 ml-10"/>
+                  </div>
+                  <div className="grid grid-cols-2 p-0 m-0 gap-1 mt-16 ml-4">
+                    <div className="h-3 w-3 bg-[#4b4e55]"></div>
+                    <div className="h-3 w-3 bg-[#4b4e55]"></div>
+                    <div className="h-3 w-3 bg-[#4b4e55]"></div>
+                    <div className="h-3 w-3 bg-[#4b4e55]"></div>
+                    <div className="h-3 w-3 bg-[#4b4e55]"></div>
+                    <div className="h-3 w-3 bg-[#4b4e55]"></div>
+                  </div>
+                </div>
+                <div className="w-[400px] h-[200px]">
+                
+                  <div className="  text-[#696f72]   -ml-20 mt-6 rounded-xl">
+                    <div className="">
+                      <ul className="menu  menu-horizontal items-center">
+                        <button className="btn bg-[#161617]  text-white border-none">Gallery</button>
+                        <button className="btn bg-[#40464e] text-[#ecedec] rounded-full border-none shadow-sm shadow-slate-50000 ml-24">+ ADD IMAGE</button>
+                        <div className="ml-10 ">
+                          <button className="p-3 shadow-xl shadow-cyan-900 text-[#6f787c] rounded-full bg-[#1d1e20]" ><FaArrowRight /></button>
+                          <button className="ml-2 shadow-xl shadow-cyan-900 text-[#6f787c] p-3 rounded-full bg-[#1d1e20]"><FaArrowLeft /></button>
+                        </div>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+               
+              </div>
+            </div>
 
           </div>
         </div>
